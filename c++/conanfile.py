3ff29703c6debf43657a16adf7f6193fefb80ffe
JimmyG <jimmy.m.gong@gmail.com>
2018-12-06 20:48:20 +0800

    - README

--- c++/conanfile.py
+++ c++/conanfile.py
@@ -14,26 +14,13 @@ class SourcemakingConan(ConanFile):
     default_options = {"shared": False, "SIMPLE": True, "boost_test:shared": False}
     generators = "cmake"
 
-#     def source(self):
-#         self.run("git clone https://github.com/memsharded/hello.git")
-#         self.run("cd hello && git checkout static_shared")
-#         # This small hack might be useful to guarantee proper /MT /MD linkage
-#         # in MSVC if the packaged project doesn't have variables to set it
-#         # properly
-#         tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-#                               '''PROJECT(MyHello)
-# include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-# conan_basic_setup()''')
-
     def requirements(self):
-        # self.requires("boost/1.68.0@{}/{}".format("conan", "stable"))
         self.requires("gtest/1.8.0@{}/{}".format("bincrafters", "stable"))
         self.requires("boost_base/1.67.0@{}/{}".format("bincrafters", "stable"))
         self.requires("boost_format/1.67.0@{}/{}".format("bincrafters", "stable"))
 
     def build(self):
         cmake = CMake(self)
-        # cmake.configure(source_folder="src")
         cmake.configure()
         # cmake.build()
         cmake.install()
@@ -49,7 +36,3 @@ class SourcemakingConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        # self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
-        # if self.options.shared:
-        #     self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
-        #     self.env_info.DYLD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
